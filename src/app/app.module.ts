4505d40ebe5564e9d0b6aa448c4fd3025f34f994
Ankit <urankitsahu92@gmail.com>
2021-08-16 08:13:07 +0530

    Gmap changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GMapModule } from 'primeng/gmap';
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+
+import { MessageService } from 'primeng/api';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GMapModule,
+    DialogModule,
+    FormsModule,
+    ButtonModule,
+    ToastModule,
+    CheckboxModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
